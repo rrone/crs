8cb7a3733ca7e91e64dafcd2595e472d16170134
Rick Roberts <web.ayso@gmail.com>
2018-09-15 12:49:03 -0700

    2018-09-14: - Updates to add config folder to dist.sh  - Updates to handle upgrade to PHP 7.2

--- dist.sh
+++ dist.sh
@@ -4,7 +4,7 @@ set -e
 #set distribution folder alias
 dist="$HOME"/GoogleDrive-rick.roberts.9/ayso/s1/web/ayso1ref/services/certrs
 config="$HOME"/Sites/AYSO/certrs/config
-PHP=/usr/local/etc/php/7.1
+PHP=/usr/local/etc/php/7.2/conf.d
 
 ## clear the screen
 echo -e "\0033"
@@ -19,9 +19,9 @@ echo
 
 echo "  Purge composer development items..."
 ## Disable xdebug for composer performance
-if [ -e $PHP"/conf.d/ext-xdebug.ini" ]
+if [ -e $PHP"/ext-xdebug.ini" ]
 then
-    mv $PHP"/conf.d/ext-xdebug.ini" $PHP"/conf.d/ext-xdebug.~ini"
+    mv $PHP"/ext-xdebug.ini" $PHP"/ext-xdebug.~ini"
 fi
 composer install --no-dev
 echo
@@ -35,6 +35,7 @@ mkdir $dist
 mkdir $dist/var
 mkdir $dist/var/uploads
 mkdir $dist/src
+mkdir $dist/config
 echo
 
 echo "  Copying app folders to distribution..."
@@ -42,7 +43,7 @@ cp -f -r app $dist/app
 cp -f -r vendor $dist/vendor
 cp -f -r public $dist/public
 cp -f -r templates $dist/templates
-##cp -f -r $config $dist/config
+cp -f -r $config/config_prod.php $dist/config/config.php
 cp -f -r src/Action $dist/src
 echo
 
@@ -65,13 +66,11 @@ echo
 
 echo "  Restore composer development items..."
 ## Restore xdebug
-if [ -e $PHP"/conf.d/ext-xdebug.~ini" ]
+if [ -e $PHP"/ext-xdebug.~ini" ]
 then
-    mv $PHP"/conf.d/ext-xdebug.~ini" $PHP"/conf.d/ext-xdebug.ini"
+    mv $PHP"/ext-xdebug.~ini" $PHP"/ext-xdebug.ini"
 fi
 composer install
 echo
 
 echo "...distribution complete"
-echo
-echo "As required, upload the config folder $config"

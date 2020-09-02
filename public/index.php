d823c82b5ea2deeb42c17508a5f36972de14740e
rick <ayso1sra@gmail.com>
2020-09-02 09:28:05 -0700

    Version 2020.09.02.00: - Upgrade to Symfony 5.1.5, etc - updated deprecated code

--- public/index.php
+++ public/index.php
@@ -35,9 +35,9 @@ $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 try {
     $response = $kernel->handle($request);
+    $response->send();
+    $kernel->terminate($request, $response);
 } catch (Exception $e) {
     echo $e->getMessage();
     die();
 }
-$response->send();
-$kernel->terminate($request, $response);

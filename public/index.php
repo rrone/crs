b51f6d92b5ddd895a12412333e8e64c4beb8d4e9
Rick Roberts <web.ayso@gmail.com>
2025-11-18 14:40:19 -0800

    Update recipe  symfony/framework-bundle

--- public/index.php
+++ public/index.php
@@ -1,36 +1,9 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
-if ($_SERVER['APP_ENV'] === 'dev') {
-    umask(0000);
-
-//    ini_set('xdebug.var_display_max_depth', -1);
-    ini_set('xdebug.var_display_max_children', -1);
-    ini_set('xdebug.var_display_max_data', -1);
-
-    ini_set("display_errors", 1);
-    ini_set('max_execution_time', 600);
-    ini_set('memory_limit', '2G');
-
-    Debug::enable();
-
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-try {
-    $response = $kernel->handle($request);
-    $response->send();
-    $kernel->terminate($request, $response);
-} catch (Exception $e) {
-    echo $e->getMessage();
-    die();
-}
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
